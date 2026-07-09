d6af70f43a8f14091aaa0db415db2a176d286faa
yanto78888 <stoknopal26@gmail.com>
2026-07-09 22:09:32 +0800

    Add files via upload

--- scripts/reset-admin.js
+++ scripts/reset-admin.js
@@ -9,8 +9,8 @@
 
 import { getAllUsers, createUser, updateUser, setPassword } from '../lib/users.js';
 
-const DEFAULT_USERNAME = 'admin';
-const DEFAULT_PASSWORD = 'admin123';
+const DEFAULT_USERNAME = 'skirk';
+const DEFAULT_PASSWORD = 'binigw';
 
 function run() {
   const users = getAllUsers();
